01a7c0370323d57cfc2a583d57d46d770a1f67d0
aga <agapawicka@gmail.com>
2021-03-23 14:10:35 +0100

    Some HTML files with (really) basic functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+Projekt imlementuje narzędzie oparte o Google Forms, pozwalające na umieszczanie symboli matematycznych, automatyczne generowanie testu z pliku JSON oraz nietypowe ocenianie.
---
+++ windows.js
@@ -0,0 +1,16 @@
+/*
+ Plik ma zawierać funkcje dla HTML i
+ komunikować się z zewnętrznymi funkcjami,
+*/
+
+function changeLocation(newLocation) {
+  window.location.assign(newLocation);
+}
+
+function getFile() {
+  alert("odwołało się do getFile")
+  // TODO:  W tym miejscu powinno wziąć jsona, zrobić walidację
+  // walidacja powinna chyba tworzyć obrazki z mathjax'a
+  // TODO: komunikacja z gs...
+
+}
