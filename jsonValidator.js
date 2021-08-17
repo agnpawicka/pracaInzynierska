137a0f8ebf29b9ed35f7f541fcc07436b1bd9eb3
aga <agapawicka@gmail.com>
2021-08-17 10:59:40 +0200

    bootstrap html + some changes with json schema

--- jsonValidator.js
+++ jsonValidator.js
@@ -3,6 +3,14 @@ module.exports = function (json){
 
   var Validator = require('jsonschema').Validator;
   var validator=  new Validator();
+  var answer = {
+    "id" : "/answer",
+    "type" : "object",
+    "properties" : {
+      "answer" : {"type" : "string"},
+      "correct" : {"type" : "boolean"}
+    }
+  };
   var question = {
     "id": "/question",
     "type": "object",
@@ -10,8 +18,11 @@ module.exports = function (json){
       "type": {"type": "string", "enum": ["checkBox"]}, //// TODO:  rodzaje pytań
       "text": {"type": "string"},
       "tex": {"type": "boolean"},
-      "answers": {"type": "array", "items": {"type": "string"}},
-      "points": {"type": "string", "enum": ["linear", "exponential"]} //// TODO: rodzaje punktacji
+      "answers": {
+        "answer" : {"type" : "string"},
+        "correct" : {"type" : "boolean"}
+      },
+      "points": {"type": "number"} //// TODO: rodzaje punktacji
     }
   };
   var schema = {
@@ -22,6 +33,7 @@ module.exports = function (json){
     "questions": {"type": "array", "items": {"$ref": "question"}
   }}}
 
+//  validator.addSchema(answer, '/answer');
   validator.addSchema(question, '/question');
   return validator.validate(json, schema).valid;
 }
